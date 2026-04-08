d865e3a58d9956d9d62007f89e4c5ea99ee3fb2b
Francis <offor919@gmail.com>
2026-04-07 22:09:30 -0700

    refactor: remove unused service module and associated configuration files

--- problemset/9. Palindrome Number.py
+++
@@ -1,9 +0,0 @@
-"""
-Given an integer x, return true if x is a palindrome, and false otherwise.
-"""
-
-class Solution:
-    def isPalindrome(self, x: int) -> bool:
-        original = str(x)
-        reverse = original[::-1]
-        return original == reverse
